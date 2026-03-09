30523fe774d9f34963fb98ebb8c233989623cc87
sugermano <1823531726@qq.com>
2026-03-09 20:15:55 +0800

    不必要信息去除

--- qa_chain/model_to_llm.py
+++
@@ -1,9 +0,0 @@
-from llm.zhipuai_llm import ZhipuAILLM
-
-def model_to_llm(model_name: str=None, temperature: float=0.6, api_key: str=None):
-    # print(model_name)
-    if 'glm' in model_name:
-        llm = ZhipuAILLM(model=model_name, zhipuai_api_key=api_key, temperature=temperature)
-    else:
-        raise ValueError(f'model {model_name} not support')
-    return llm
